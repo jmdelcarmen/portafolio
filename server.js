f2b440f420301a2e1b1dc2ee6581aaa2bc5142e4
Marco del Carmen <jesusmarcodelcarmen@gmail.com>
2017-04-16 17:46:59 -0500

    Added contact form to contact panel.

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.static(__dirname));
+app.use(bodyParser());
+app.use(bodyParser.urlencoded({extended: false}));
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));
+});
+app.post('/message', (req, res) => {
 });
 
 app.listen(3000, () => {
--- src/components/panels/contact.js
+++ src/components/panels/contact.js
@@ -2,6 +2,34 @@ export default `
   <div id="contact">
     <h1><i class="fa fa-address-book-o"></i><span>contact</span></h1>
     <div class="content">
-      <h3 class="text-center">coming soon.</h3>
+      <h3 class="text-center">let me know what you think</h3>
+      <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+          <form method="post" action="message">
+            <div class='form-group'>
+              <div class="col-md-6">
+                <label><i class="fa fa-user"></i> name</label>
+                <input name="subj" class="col-sm-12 form-control" type="text" placeholder="enter your name..."/>
+              </div>
+              <div class="col-md-6">
+                <label><i class="fa fa-envelope"></i> email</label>
+                <input name="email" class="col-sm-12 form-control" type="text" placeholder="enter your a subject..."/>
+              </div>
+              <div class="col-md-12">
+                <label><i class="fa fa-map-pin"></i> subject</label>
+                <input name="name" class="col-sm-12 form-control" type="text" placeholder="enter your a subject..."/>
+              </div>
+            </div>
+            <div class="col-md-12">
+              <label><i class="fa fa-commenting"></i> content</label>
+              <textarea class="form-control" name="content" placeholder="type something..." value=""></textarea>
+            </div>
+            <br>
+            <div class="card-buttons">
+              <button><i class="fa fa-angle-double-right"></i><strong>send</strong></button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   </div>`;
